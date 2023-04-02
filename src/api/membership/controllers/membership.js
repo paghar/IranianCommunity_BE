5bdccdd7d5ee3748ff0750a1baf7b05ed2f95139
fatemeh paghar <f.paghar@gmail.com>
2023-04-02 20:55:06 +0430

    finish content type schema

--- src/api/membership/controllers/membership.js
+++ src/api/membership/controllers/membership.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * event controller
+ * membership controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::event.event');
+module.exports = createCoreController('api::membership.membership');
--- src/api/plan/controllers/plan.js
+++ src/api/plan/controllers/plan.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * project controller
+ * plan controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::project.project');
+module.exports = createCoreController('api::plan.plan');
