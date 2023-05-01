ff3b876114c8d3fd79937b96fb364c53fb66cb67
fatemeh paghar <f.paghar@gmail.com>
2023-05-01 17:01:46 +0430

    change findOne controller to get plan by slug

--- src/api/plan/controllers/plan.js
+++ src/api/plan/controllers/plan.js
@@ -4,6 +4,21 @@
  * plan controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::plan.plan');
+module.exports = createCoreController("api::plan.plan", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const query = {
+      filters: { slug },
+      ...ctx.query,
+    };
+
+    const plan = await strapi.entityService.findMany("api::plan.plan", query);
+
+    const sanitizedEntity = await this.sanitizeOutput(plan);
+
+    return this.transformResponse(sanitizedEntity[0]);
+  },
+}));
--- src/api/plan/routes/plan.js
+++ src/api/plan/routes/plan.js
@@ -6,4 +6,48 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::plan.plan');
+const defaultRouter = createCoreRouter("api::plan.plan");
+
+// function to add to or override default router methods
+const customRouter = (innerRouter, routeOveride = [], extraRoutes = []) => {
+  let routes;
+
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes;
+
+      const newRoutes = routes.map((route) => {
+        let found = false;
+
+        routeOveride.forEach((overide) => {
+          if (
+            route.handler === overide.handler &&
+            route.method === overide.method
+          ) {
+            found = overide;
+          }
+        });
+
+        return found || route;
+      });
+
+      return newRoutes.concat(extraRoutes);
+    },
+  };
+};
+
+// Overide the default router with the custom router to use slug.
+const myOverideRoutes = [
+  {
+    method: "GET",
+    path: "/plans/:slug",
+    handler: "api::plan.plan.findOne",
+  },
+];
+
+const myExtraRoutes = [];
+
+module.exports = customRouter(defaultRouter, myOverideRoutes, myExtraRoutes);
