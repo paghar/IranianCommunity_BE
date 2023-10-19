df48ef23145a60d36f72fff2e68645207813f471
fpaghar <f.paghar@gmail.com>
2023-10-19 09:25:46 +0200

    feat:adding EN local and community table

---
+++ src/api/community-info/services/community-info.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * community-info service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::community-info.community-info');
